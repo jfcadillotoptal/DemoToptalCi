0b02c78b144e967d046136c416961a3b233526a2
Franco Cadillo <franco.cadillo@toptal.com>
2022-08-24 18:27:52 -0500

    Work In Progress - Savepoint

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -6,3 +6,4 @@ xcodebuild -workspace DemoToptalCI.xcworkspace \
             -scheme DemoToptalCI \
             -destination platform=iOS\ Simulator,OS=15.0,name=iPhone\ 11 \
             clean test | xcpretty
+
