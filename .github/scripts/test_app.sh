078b96e0ca248203199767316e7d8a25b05604b3
Franco Cadillo <franco.cadillo@toptal.com>
2022-08-25 00:05:27 -0500

    Work In Progress - Savepoint

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -7,5 +7,9 @@ set -eo pipefail
 #            -destination platform=iOS\ Simulator,OS=15.0,name=iPhone\ 12 \
 #            clean test | xcpretty
 
-xcodebuild test -workspace DemoToptalCI.xcworkspace -scheme DemoToptalCI -destination 'platform=iOS Simulator,name=iPhone 12,OS=15.0'
+#xcodebuild test -workspace DemoToptalCI.xcworkspace -scheme DemoToptalCI -destination 'platform=iOS Simulator,name=iPhone 12,OS=15.0'
 
+xcodebuild test \
+-workspace DemoToptalCI.xcworkspace \
+-scheme DemoToptalCI \
+-destination platform=iOS\ Simulator,OS=15.0,name=iPhone\ 12
