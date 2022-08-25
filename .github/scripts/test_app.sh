2500cfdc9d8154c4e30cfdb7e61c561639bee571
Franco Cadillo <franco.cadillo@toptal.com>
2022-08-24 19:16:05 -0500

    Work In Progress - Savepoint

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -5,5 +5,5 @@ set -eo pipefail
 xcodebuild -workspace DemoToptalCI.xcworkspace \
             -scheme DemoToptalCI \
             -sdk iphonesimulator \
-            -destination 'platform=iOS Simulator,name=iPhone 12,OS=15.0'
+            -destination platform=iOS\ Simulator,name=iPhone\ 12,OS=15.0 \
 
