ab825cdf793214a14dd8c1e7f847a9b26a3b66ab
Franco Cadillo <franco.cadillo@toptal.com>
2022-08-25 11:34:48 -0500

    Work In Progress - Savepoint

--- DemoToptalCITests/DemoToptalCITests.swift
+++ DemoToptalCITests/DemoToptalCITests.swift
@@ -32,13 +32,7 @@ class DemoToptalCITests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        XCTFail("Sample Unit test fail")
     }
 
 }
