b8d08fa07a4a5ed47661ea96543e4704ba3d4fac
Franco Cadillo <franco.cadillo@toptal.com>
2022-08-25 11:27:27 -0500

    Work In Progress - Savepoint

--- DemoToptalCITests/DemoToptalCITests.swift
+++ DemoToptalCITests/DemoToptalCITests.swift
@@ -25,7 +25,6 @@ class DemoToptalCITests: XCTestCase {
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     
-        
     }
     
     func testUnitTestExampleFail() throws {
